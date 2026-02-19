6227eef3dac0b4340776498d60a4142483c93afa
Navdeep Singh <navsingh@paymentus.com>
2026-02-19 09:20:20 +0530

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Material(
-        child: Container(
-          child: const Center(
-            child: Text("Welcome to Nav way of learning flutter in 30 days")
-            )
-          ),
-      ),
+      home: Home(),
     );
   }
-}
+} 
