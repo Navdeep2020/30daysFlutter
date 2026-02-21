f7a690950fca19ad5e3f237eb063acf638bd7a40
Navdeep Singh <navsingh@paymentus.com>
2026-02-21 21:41:17 +0530

    day 3 done

--- lib/home.dart
+++ lib/home.dart
@@ -1,24 +1,21 @@
 import "package:flutter/material.dart";
 
 class Home extends StatelessWidget {
- // const Home()({super.key});
+  // const Home()({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final int days = 30;
 
     return Scaffold(
       appBar: AppBar(
         title: Text("Heading"),
         backgroundColor: Colors.blue,
-        ),
-        body: Center(
+      ),
+      body: Center(
           child: Container(
-            child: Text("Welcome to Navdeep way of learning flutter in $days dayss")
-            )
-          ),
-          drawer: Drawer(),
-      );
+              child: Text("Welcome to learning flutter in $days dayss"))),
+      drawer: Drawer(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/home.dart';
+import 'package:flutter_catalog/login.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home: Home(),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => Home(),
+         "/login": (context) => Login()
+      },
     );
   }
-} 
+}
