ddfec990df82db652cb008d5f843795546933101
Navdeep Singh <navsingh@paymentus.com>
2026-03-17 09:25:12 +0530

    View redrawing

--- lib/login.dart
+++ lib/login.dart
@@ -1,9 +1,16 @@
 import "package:flutter/material.dart";
 import "package:flutter_catalog/utils/routes.dart";
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
 
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+  String name = "";
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -21,8 +28,8 @@ class Login extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              const Text(
-                "Welcome",
+               Text(
+                "Welcome $name",
                 style: TextStyle(
                   fontSize: 22,
                   fontWeight: FontWeight.bold
@@ -37,6 +44,11 @@ class Login extends StatelessWidget {
                           hintText: "Enter username",
                           labelText: "Username"
                         ),
+                        onChanged: (value) {
+                          name = value;
+                          // redraws the view again
+                          setState(() {});
+                        },
                       ),
                       TextFormField(
                         obscureText: false,
@@ -48,13 +60,13 @@ class Login extends StatelessWidget {
                       const SizedBox(
                         height: 22,
                       ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, MyRoutes.homeRoute);
-                        },
-                         child: const Text(
-                          "Login")
-                      )
+                      // ElevatedButton(
+                      //   onPressed: () {
+                      //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      //   },
+                      //    child: const Text(
+                      //     "Login")
+                      // )
                     ],
                   ),
                 )
